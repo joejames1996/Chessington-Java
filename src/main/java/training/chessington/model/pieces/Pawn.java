c8e209ec5df097af988297061003f2bdac416f8d
Joe James <Joe.James@softwire.com>
2018-09-14 14:18:27 +0100

    Made pawns move two spaces on first try

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -17,8 +17,16 @@ public class Pawn extends AbstractPiece {
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
         List<Move> allowedMoves = new ArrayList<>();
         int allowedColMovement = this.colour == PlayerColour.BLACK ? 1 : -1;
-        
+        int allowedFirstColMovement = this.colour == PlayerColour.BLACK ? 2 : -2;
+
         allowedMoves.add(new Move(from, from.plus(allowedColMovement, 0)));
+        if(!hasPawnMoved(from)) allowedMoves.add(new Move(from, from.plus(allowedFirstColMovement, 0)));
         return allowedMoves;
+    }
+
+    public boolean hasPawnMoved(Coordinates pos)
+    {
+        int startingRow = this.colour == PlayerColour.BLACK ? 1 : 6;
+        return pos.getRow() == startingRow ? false : true;
     }
 }
