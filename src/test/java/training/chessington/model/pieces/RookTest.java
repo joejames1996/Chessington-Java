be9a2d347581049ebe4ec3e5079351fa2971a77d
Joe James <Joe.James@softwire.com>
2018-09-20 14:08:08 +0100

    Rook tests

--- src/test/java/training/chessington/model/pieces/RookTest.java
+++ src/test/java/training/chessington/model/pieces/RookTest.java
@@ -1,7 +1,47 @@
 package training.chessington.model.pieces;
 
+import org.junit.Test;
+import training.chessington.model.Board;
+import training.chessington.model.Coordinates;
+import training.chessington.model.Move;
+import training.chessington.model.PlayerColour;
+
+import java.util.List;
+
 import static training.chessington.model.pieces.PieceAssert.*;
 import static org.assertj.core.api.Assertions.*;
 
-public class RookTest {
+public class RookTest
+{
+    @Test
+    public void whiteRookCanMove()
+    {
+        Board board = Board.empty();
+        Piece rook = new Rook(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(6, 4);
+        board.placePiece(coords, rook);
+
+        List<Move> moves = rook.getAllowedMoves(coords, board);
+
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, 0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(1, 0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(0, -1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(0, 1)));
+    }
+
+    @Test
+    public void blackRookCanMove()
+    {
+        Board board = Board.empty();
+        Piece rook = new Rook(PlayerColour.BLACK);
+        Coordinates coords = new Coordinates(6, 4);
+        board.placePiece(coords, rook);
+
+        List<Move> moves = rook.getAllowedMoves(coords, board);
+
+        assertThat(moves).contains(new Move(coords, coords.plus(-1, 0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(1, 0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(0, -1)));
+        assertThat(moves).contains(new Move(coords, coords.plus(0, 1)));
+    }
 }
