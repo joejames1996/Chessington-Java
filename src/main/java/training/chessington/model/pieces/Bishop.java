5e9af021277cfba6b1414ed563350873843d6dd0
Joe James <Joe.James@softwire.com>
2018-09-20 16:58:33 +0100

    Bishop movement + tests

--- src/main/java/training/chessington/model/pieces/Bishop.java
+++ src/main/java/training/chessington/model/pieces/Bishop.java
@@ -14,7 +14,99 @@ public class Bishop extends AbstractPiece {
     }
 
     @Override
-    public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+    public List<Move> getAllowedMoves(Coordinates from, Board board)
+    {
+        List<Move> allowedMoves = new ArrayList<>();
+
+        allowedMoves.addAll(addUpLeftMoves(from, board));
+        allowedMoves.addAll(addUpRightMoves(from, board));
+        allowedMoves.addAll(addDownLeftMoves(from, board));
+        allowedMoves.addAll(addDownRightMoves(from, board));
+
+        return allowedMoves;
+    }
+
+    public List<Move> addUpLeftMoves(Coordinates from, Board board)
+    {
+        List<Move> allowedMoves = new ArrayList<>();
+        int currentRow = from.getRow();
+        int currentCol = from.getCol();
+
+        while(currentRow != 0 && currentCol != 0)
+        {
+            currentRow--; currentCol--;
+            Coordinates coordinates = new Coordinates(currentRow, currentCol);
+            if(board.get(coordinates) != null)
+            {
+                if(board.get(coordinates).getColour() != this.colour)
+                    allowedMoves.add(new Move(from, coordinates));
+                break;
+            }
+            allowedMoves.add(new Move(from, coordinates));
+        }
+        return allowedMoves;
+    }
+
+    public List<Move> addUpRightMoves(Coordinates from, Board board)
+    {
+        List<Move> allowedMoves = new ArrayList<>();
+        int currentRow = from.getRow();
+        int currentCol = from.getCol();
+
+        while(currentRow != 0 && currentCol != 7)
+        {
+            currentRow--; currentCol++;
+            Coordinates coordinates = new Coordinates(currentRow, currentCol);
+            if(board.get(coordinates) != null)
+            {
+                if(board.get(coordinates).getColour() != this.colour)
+                    allowedMoves.add(new Move(from, coordinates));
+                break;
+            }
+            allowedMoves.add(new Move(from, coordinates));
+        }
+        return allowedMoves;
+    }
+
+    public List<Move> addDownLeftMoves(Coordinates from, Board board)
+    {
+        List<Move> allowedMoves = new ArrayList<>();
+        int currentRow = from.getRow();
+        int currentCol = from.getCol();
+
+        while(currentRow != 7 && currentCol != 0)
+        {
+            currentRow++; currentCol--;
+            Coordinates coordinates = new Coordinates(currentRow, currentCol);
+            if(board.get(coordinates) != null)
+            {
+                if(board.get(coordinates).getColour() != this.colour)
+                    allowedMoves.add(new Move(from, coordinates));
+                break;
+            }
+            allowedMoves.add(new Move(from, coordinates));
+        }
+        return allowedMoves;
+    }
+
+    public List<Move> addDownRightMoves(Coordinates from, Board board)
+    {
+        List<Move> allowedMoves = new ArrayList<>();
+        int currentRow = from.getRow();
+        int currentCol = from.getCol();
+
+        while(currentRow != 7 && currentCol != 7)
+        {
+            currentRow++; currentCol++;
+            Coordinates coordinates = new Coordinates(currentRow, currentCol);
+            if(board.get(coordinates) != null)
+            {
+                if(board.get(coordinates).getColour() != this.colour)
+                    allowedMoves.add(new Move(from, coordinates));
+                break;
+            }
+            allowedMoves.add(new Move(from, coordinates));
+        }
+        return allowedMoves;
     }
 }
--- src/test/java/training/chessington/model/pieces/BishopTest.java
+++ src/test/java/training/chessington/model/pieces/BishopTest.java
@@ -1,7 +1,88 @@
 package training.chessington.model.pieces;
 
+import org.junit.Test;
+import training.chessington.model.Board;
+import training.chessington.model.Coordinates;
+import training.chessington.model.Move;
+import training.chessington.model.PlayerColour;
+
+import java.util.List;
+
 import static training.chessington.model.pieces.PieceAssert.*;
 import static org.assertj.core.api.Assertions.*;
 
-public class BishopTest {
+public class BishopTest
+{
+    @Test
+    public void bishopsCanMoveDiagonally()
+    {
+        Board board = Board.empty();
+
+        Piece bishop = new Bishop(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(2, 2);
+        board.placePiece(coords, bishop);
+
+        List<Move> bishopMoves = bishop.getAllowedMoves(coords, board);
+
+        assertThat(bishopMoves).contains(new Move(coords, coords.plus(1, 1)));
+        assertThat(bishopMoves).contains(new Move(coords, coords.plus(-1, 1)));
+        assertThat(bishopMoves).contains(new Move(coords, coords.plus(1, -1)));
+        assertThat(bishopMoves).contains(new Move(coords, coords.plus(-1, -1)));
+    }
+
+    @Test
+    public void bishopsCannotMoveIfFriendlyPieceInFront() {
+        Board board = Board.empty();
+
+        Piece firstWhiteBishop = new Bishop(PlayerColour.WHITE);
+        Coordinates firstWhiteCoords = new Coordinates(4, 4);
+        board.placePiece(firstWhiteCoords, firstWhiteBishop);
+
+        Piece secondWhiteBishop = new Bishop(PlayerColour.WHITE);
+        Coordinates secondWhiteCoords = new Coordinates(5, 5);
+        board.placePiece(secondWhiteCoords, secondWhiteBishop);
+
+        List<Move> firstWhiteMoves = firstWhiteBishop.getAllowedMoves(firstWhiteCoords, board);
+        List<Move> secondWhiteMoves = secondWhiteBishop.getAllowedMoves(secondWhiteCoords, board);
+
+        assertThat(firstWhiteMoves).doesNotContain(new Move(firstWhiteCoords, secondWhiteCoords.plus(1, 1)));
+        assertThat(secondWhiteMoves).doesNotContain(new Move(firstWhiteCoords, secondWhiteCoords.plus(-1, -1)));
+    }
+
+    @Test
+    public void bishopsCannotMoveTwoSquaresIfPieceInFront() {
+        Board board = Board.empty();
+
+        Piece blackBishop = new Bishop(PlayerColour.BLACK);
+        Coordinates blackCoords = new Coordinates(2, 2);
+        board.placePiece(blackCoords, blackBishop);
+
+        Piece whiteBishop= new Bishop(PlayerColour.WHITE);
+        Coordinates whiteCoords = new Coordinates(3, 3);
+        board.placePiece(whiteCoords, whiteBishop);
+
+        List<Move> blackMoves = blackBishop.getAllowedMoves(blackCoords, board);
+        List<Move> whiteMoves = whiteBishop.getAllowedMoves(whiteCoords, board);
+
+        assertThat(blackMoves).doesNotContain(new Move(blackCoords, blackCoords.plus(2, 2)));
+        assertThat(whiteMoves).doesNotContain(new Move(blackCoords, blackCoords.plus(-2, -2)));
+    }
+
+    @Test
+    public void bishopsCannotMoveOutsideBoard() {
+        Board board = Board.empty();
+        Piece bishop = new Bishop(PlayerColour.BLACK);
+        Coordinates coords = new Coordinates(0, 0);
+        board.placePiece(coords, bishop);
+
+        List<Move> moves = bishop.getAllowedMoves(coords, board);
+
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-1, -1)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(1, -1)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-1, 1)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(8, 8)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-8, 8)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(8, -8)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-8, -8)));
+    }
 }
