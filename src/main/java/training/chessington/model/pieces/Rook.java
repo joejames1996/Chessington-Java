6b92b6e3afa485a55ef01aa04eb4f5f09d98b1cb
Joe James <Joe.James@softwire.com>
2018-09-20 14:31:05 +0100

    Rook can move

--- src/main/java/training/chessington/model/pieces/Rook.java
+++ src/main/java/training/chessington/model/pieces/Rook.java
@@ -8,13 +8,75 @@ import training.chessington.model.PlayerColour;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Rook extends AbstractPiece {
-    public Rook(PlayerColour colour) {
+public class Rook extends AbstractPiece
+{
+    public Rook(PlayerColour colour)
+    {
         super(PieceType.ROOK, colour);
     }
 
     @Override
-    public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+    public List<Move> getAllowedMoves(Coordinates from, Board board)
+    {
+        List<Move> allowedMoves = new ArrayList<>();
+
+        allowedMoves = addUpwardMoves(allowedMoves, from);
+        allowedMoves = addDownwardMoves(allowedMoves, from);
+        allowedMoves = addLeftMoves(allowedMoves, from);
+        allowedMoves = addRightMoves(allowedMoves, from);
+
+        return allowedMoves;
+    }
+
+    public List<Move> addUpwardMoves(List<Move> allowedMoves, Coordinates from)
+    {
+        int currentRow = from.getRow();
+        int currentCol = from.getCol();
+
+        while(currentRow != 0)
+        {
+            currentRow--;
+            allowedMoves.add(new Move(from, new Coordinates(currentRow, currentCol)));
+        }
+        return allowedMoves;
+    }
+
+    public List<Move> addDownwardMoves(List<Move> allowedMoves, Coordinates from)
+    {
+        int currentRow = from.getRow();
+        int currentCol = from.getCol();
+
+        while(currentRow != 7)
+        {
+            currentRow++;
+            allowedMoves.add(new Move(from, new Coordinates(currentRow, currentCol)));
+        }
+        return allowedMoves;
+    }
+
+    public List<Move> addLeftMoves(List<Move> allowedMoves, Coordinates from)
+    {
+        int currentRow = from.getRow();
+        int currentCol = from.getCol();
+
+        while(currentCol != 0)
+        {
+            currentCol--;
+            allowedMoves.add(new Move(from, new Coordinates(currentRow, currentCol)));
+        }
+        return allowedMoves;
+    }
+
+    public List<Move> addRightMoves(List<Move> allowedMoves, Coordinates from)
+    {
+        int currentRow = from.getRow();
+        int currentCol = from.getCol();
+
+        while(currentCol != 7)
+        {
+            currentCol++;
+            allowedMoves.add(new Move(from, new Coordinates(currentRow, currentCol)));
+        }
+        return allowedMoves;
     }
 }
