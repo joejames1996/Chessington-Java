56b887b1990a3011dbd820764c6230506ba1043e
Joe James <Joe.James@softwire.com>
2018-09-20 11:59:35 +0100

    Refactor + Pawns can't go beyond edge of board

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -5,7 +5,12 @@ import training.chessington.model.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Pawn extends AbstractPiece {
+public class Pawn extends AbstractPiece
+{
+    int allowedColMovement = this.colour == PlayerColour.BLACK ? 1 : -1;
+    int allowedFirstColMovement = this.colour == PlayerColour.BLACK ? 2 : -2;
+    int startingRow = this.colour == PlayerColour.BLACK ? 1 : 6;
+
     public Pawn(PlayerColour colour) {
         super(Piece.PieceType.PAWN, colour);
     }
@@ -13,20 +18,32 @@ public class Pawn extends AbstractPiece {
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
         List<Move> allowedMoves = new ArrayList<>();
-        int allowedColMovement = this.colour == PlayerColour.BLACK ? 1 : -1;
-        int allowedFirstColMovement = this.colour == PlayerColour.BLACK ? 2 : -2;
 
-        allowedMoves.add(new Move(from, from.plus(allowedColMovement, 0)));
+        allowedMoves = addNewMove(allowedMoves, from, from.plus(allowedColMovement, 0));
         if(!hasPawnMoved(from)) allowedMoves.add(new Move(from, from.plus(allowedFirstColMovement, 0)));
 
-        allowedMoves = checkPiecesAreNotInFront(allowedMoves, board);
+        allowedMoves = checkMoveListValidity(allowedMoves, board);
         return allowedMoves;
     }
 
     public boolean hasPawnMoved(Coordinates pos)
     {
-        int startingRow = this.colour == PlayerColour.BLACK ? 1 : 6;
         return pos.getRow() == startingRow ? false : true;
+    }
+
+    public List<Move> addNewMove(List<Move> moveList, Coordinates from, Coordinates to)
+    {
+        if(!(to.getRow() < 0 || to.getRow() > 7 || to.getCol() < 0 || to.getCol() > 7))
+        {
+            moveList.add(new Move(from, to));
+        }
+        return moveList;
+    }
+
+    public List<Move> checkMoveListValidity(List<Move> moveList, Board board)
+    {
+        moveList = checkPiecesAreNotInFront(moveList, board);
+        return moveList;
     }
 
     public List<Move> checkPiecesAreNotInFront(List<Move> moveList, Board board)
